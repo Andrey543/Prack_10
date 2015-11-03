2977b29b2c9c2ef613a9c0e01e0e1911b2fe8fa9
Andrey543 <andrey_rybakov_97@mail.ru>
2015-11-03 11:38:55 +0300

    Exersise #5

--- 5.py
+++ 5.py
@@ -5,7 +5,7 @@ slovar={}
 while len(s)>1:
     slovar[s[:s.index(' - ')]]=s[s.index(' - ')+3:s.index('\n')]
     s=s[s.index('\n')+1:]
-vyv=[x+' - '+slovar[x] for x in slovar]
+vyv=[slovar[x]+' - '+x for x in slovar]
 vyv=sorted(vyv)
 for i in range(len(slovar)):
     print(vyv[i], file=output)
